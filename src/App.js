d85625615122eeff5ca27e90c8c94e2a44bb6f0c
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-30 19:06:00 +0200

    issue react-router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	const [data, setData] = useState([]);
 
 	const getData = async () => {
-		await fetch("FEM-Time_tracking_dashboard_REACT/data.json", {
+		await fetch("./data.json", {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
@@ -30,10 +30,10 @@ function App() {
 		<div className="App">
 			<Layout>
 				<Routes>
-					<Route path="/FEM-Time_tracking_dashboard_REACT" element={<Navigate replace to="/FEM-Time_tracking_dashboard_REACT/weekly-report" />}></Route>
-					<Route path="/FEM-Time_tracking_dashboard_REACT/daily-report" element={<DailyReport data={data} />}></Route>
-					<Route path="/FEM-Time_tracking_dashboard_REACT/weekly-report" element={<WeeklyReport data={data} />}></Route>
-					<Route path="/FEM-Time_tracking_dashboard_REACT/monthly-report" element={<MonthlyReport data={data} />}></Route>
+					<Route path="/" element={<Navigate replace to="/weekly-report" />}></Route>
+					<Route path="/daily-report" element={<DailyReport data={data} />}></Route>
+					<Route path="/weekly-report" element={<WeeklyReport data={data} />}></Route>
+					<Route path="/monthly-report" element={<MonthlyReport data={data} />}></Route>
 				</Routes>
 			</Layout>
 		</div>
