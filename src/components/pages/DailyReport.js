3a1f1cbdf899ec184c8fe074882d2f4119f21683
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-30 17:24:42 +0200

    updated fetch json

--- src/components/pages/DailyReport.js
+++ src/components/pages/DailyReport.js
@@ -5,7 +5,7 @@ const DailyReport = () => {
 	const [data, setData] = useState([]);
 
 	const getData = async () => {
-		await fetch("%PUBLIC_URL%/data.json", {
+		await fetch("./data.json", {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
